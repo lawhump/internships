87b7e98fea04daf0efbac338694c2a1b05489b39
Lawrence Humphrey <lawrence@lawrences-mbp-2.austin.ibm.com>
2016-02-18 12:29:16 -0600

    something's wrong with the build but committing anyway

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+// var gulp = require('gulp');
+// var $    = require('gulp-load-plugins')();
+//
+var sassPaths = [
+  'bower_components/foundation-sites/scss',
+  'bower_components/motion-ui/src'
+];
+//
+// gulp.task('sass', function() {
+//   return gulp.src('scss/app.scss')
+//     .pipe($.sass({
+//       includePaths: sassPaths
+//     })
+//       .on('error', $.sass.logError))
+//     .pipe($.autoprefixer({
+//       browsers: ['last 2 versions', 'ie >= 9']
+//     }))
+//     .pipe(gulp.dest('css'));
+// });
+//
+// gulp.task('default', ['sass'], function() {
+//   gulp.watch(['scss/**/*.scss'], ['sass']);
+// });
+
+var gulp = require('gulp');
+var $    = require('gulp-load-plugins')();
+var rename = require('gulp-rename');
+var cssnano = require('gulp-cssnano');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var server = require('gulp-webserver');
+
+
+// Concatenate and compile SCSS
+gulp.task('sass', function() {
+  return gulp.src('app/scss/app.scss')
+    .pipe($.sass({
+        includePaths: sassPaths
+      })
+      .on('error', $.sass.logError))
+    .pipe($.autoprefixer({
+      browsers: ['last 2 versions', 'ie >= 9']
+    }))
+    .pipe(gulp.dest('app/css'))
+    .pipe(cssnano())
+    .pipe(rename('all.min.css'))
+    .pipe(gulp.dest('dist/css'));
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src(['app/js/**/*.js', '!dev/js/vendor/**'])
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('default', ['sass', 'scripts'], function() {
+  gulp.watch(['app/scss/**/*.scss'], ['sass']);
+  gulp.watch('app/js/*.js', ['scripts']);
+});
+
+gulp.task('build', ['sass', 'scripts']);
