97af57192c9b768ee5120e599f43aa8add75b146
Noah Lafferty <noah.lafferty@gmail.com>
2016-02-18 14:20:25 -0600

    added faceswap

--- dist/js/all.js
+++ dist/js/all.js
@@ -1,1 +1,38 @@
 $(document).foundation();
+
+var sliderSettings = {
+	item: 4,
+	loop: true,
+	controls: false,
+	pager: false,
+    onBeforeSlide: resetTimer,
+	responsive: [
+		{
+            breakpoint:1100,
+            settings: {
+                item:2
+            }
+        },
+        {
+            breakpoint:600,
+            settings: {
+                item:1
+          	}	
+        }
+	]
+}
+
+var sliderTop = $('#top-slider').lightSlider(sliderSettings);
+var sliderBot = $('#bottom-slider').lightSlider(sliderSettings);
+
+var shuffleTimer = setTimeout(shuffle, 3000);
+
+function shuffle() {
+	sliderTop.goToSlide(Math.floor(Math.random() * 9));
+	sliderBot.goToSlide(Math.floor(Math.random() * 9));
+}
+
+function resetTimer() {
+    clearTimeout(shuffleTimer);
+    shuffleTimer = setTimeout(shuffle, 3000);
+}
