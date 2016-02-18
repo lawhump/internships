a4de8d441c2fa6e0cf48cd7883cd787a8236e982
Lawrence Humphrey <lawrenceh1993@gmail.com>
2016-02-18 16:31:52 -0600

    pulling

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,22 @@
 $(document).foundation();
+
+$(document).ready(function() {
+  // var $element = $('#example');
+  // var elem = new Foundation.Magellan($element);
+});
+
+$(window).scroll(function(){
+  var bottom_of_nav = $('.nav').offset().top + $(this).outerHeight();
+  var bottom_of_roles = $('.roles').offset().top + $(this).outerHeight();
+  var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+  if (bottom_of_window > bottom_of_roles) {
+    $('.roles > div').each(function(i) {
+      $(this).animate({'opacity':'1'},500);
+    });
+  }
+
+  // if (bottom_of_window > bottom_of_nav) {
+  //   $('.nav').addClass('fixed');
+  // }
+});
