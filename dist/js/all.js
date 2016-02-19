6cb4f000ec3cc407d39f3682c7e1f64465d6e267
Lawrence Humphrey <lawrenceh1993@gmail.com>
2016-02-18 18:35:11 -0600

    slightly fixed footer

--- dist/js/all.js
+++ dist/js/all.js
@@ -1,54 +1,57 @@
-function shuffle() {
-	sliderTop.goToSlide(Math.floor(Math.random() * 9));
-	sliderBot.goToSlide(Math.floor(Math.random() * 9));
-}
-
-function resetTimer() {
-    clearTimeout(shuffleTimer);
-    shuffleTimer = setTimeout(shuffle, 4000);
-}
-
 $(document).foundation();
 
 $(document).ready(function() {
   // var $element = $('#example');
   // var elem = new Foundation.Magellan($element);
+
   $('#hero').twentytwenty({
-    default_offset_pct: 0
+  	default_offset_pct: 0
   });
 
   var sliderSettings = {
-    item: 4,
-    loop: true,
-    controls: false,
-    pager: false,
-    onBeforeSlide: resetTimer,
-    responsive: [
-      {
-        breakpoint:1200,
-        settings: {
-          item:3
-        }
-      },
-      {
-        breakpoint:850,
-        settings: {
-          item:2
-        }
-      },
-      {
-        breakpoint:500,
-        settings: {
-          item:1
-        }
-      }
-    ]
+  	item: 4,
+  	loop: true,
+  	controls: false,
+  	pager: false,
+      onBeforeSlide: resetTimer,
+  	responsive: [
+  		{
+              breakpoint:1200,
+              settings: {
+                  item:3
+              }
+          },
+          {
+              breakpoint:850,
+              settings: {
+                  item:2
+              }
+          },
+          {
+              breakpoint:500,
+              settings: {
+                  item:1
+            	}
+          }
+  	]
   }
 
   var sliderTop = $('#top-slider').lightSlider(sliderSettings);
   var sliderBot = $('#bottom-slider').lightSlider(sliderSettings);
 
   var shuffleTimer = setTimeout(shuffle, 4000);
+
+  var shuffle = function() {
+  	sliderTop.goToSlide(Math.floor(Math.random() * 9));
+  	sliderBot.goToSlide(Math.floor(Math.random() * 9));
+  };
+
+  var resetTimer = function() {
+      clearTimeout(shuffleTimer);
+      shuffleTimer = setTimeout(shuffle, 4000);
+  };
+
+  $('.shuffle').on('click', shuffle);
 });
 
 $(window).scroll(function(){
