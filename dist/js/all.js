570a31e5a55edf1b3cd4c13148712081943a6f86
Lawrence Humphrey <lawrenceh1993@gmail.com>
2016-02-19 10:03:15 -0600

    small tweaks and shit

--- dist/js/all.js
+++ dist/js/all.js
@@ -17,9 +17,6 @@ var scroll = function() {
   var top_of_sessions = $('.sessions').offset().top;
   var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-  console.log('top_of_roles='+top_of_roles);
-  console.log('bottom_of_window='+bottom_of_window);
-
   var margin = 170;
   // Hard coded. I know there's a more elegant way but we only have two days
   if (bottom_of_window > (top_of_roles+margin)) {
