b0b6891b31c69c51ad5e07fc974631b6af055a3e
Noah Lafferty <noah.lafferty@gmail.com>
2016-02-19 10:38:33 -0600

    sped up scrolling in nav

--- dist/js/all.js
+++ dist/js/all.js
@@ -106,7 +106,7 @@ $('a[href*="#"]:not([href="#"])').click(function() {
     if (target.length) {
       $('html, body').animate({
         scrollTop: target.offset().top - $('.nav').height()
-      }, 1000);
+      }, 400);
       return false;
     }
   }
